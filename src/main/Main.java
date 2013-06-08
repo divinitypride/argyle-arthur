e8686e08d287f871c3af1d1538c17c11dc8095be
Jarrod Hollis <jjih73@gmail.com>
2013-06-08 10:47:15 -0700

    Cleanup and organization. Removed some junk files.

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,130 +19,104 @@ import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
  
 public class Main extends SimpleApplication
-        implements ActionListener {
+  implements ActionListener {
  
-  private Spatial sceneModel;
-  private BulletAppState bulletAppState;
-  private RigidBodyControl landscape;
-  private CharacterControl player;
-  private Vector3f walkDirection = new Vector3f();
-  private boolean left = false, right = false, up = false, down = false;
+    private Spatial sceneModel;
+    private BulletAppState bulletAppState;
+    private RigidBodyControl landscape;
+    private CharacterControl player;
+    private Vector3f walkDirection = new Vector3f();
+    private boolean left = false, right = false, up = false, down = false;
  
-  public static void main(String[] args) {
-    Main app = new Main();
-    app.start();
-  }
- 
-  public void simpleInitApp() {
-    // Set up Physics
-    bulletAppState = new BulletAppState();
-    stateManager.attach(bulletAppState);
-    bulletAppState.getPhysicsSpace().enableDebug(assetManager);
- 
-    viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
-    
-    FilterPostProcessor filter = new FilterPostProcessor(assetManager);
-    FogFilter fog = new FogFilter();
-    fog.setFogColor(new ColorRGBA(0.9f, 0.9f, 0.9f, 1.0f));
-    fog.setFogDistance(512);
-    fog.setFogDensity(1.5f);
-    filter.addFilter(fog);
-    viewPort.addProcessor(filter);
-    
-    flyCam.setMoveSpeed(100);
-    setUpKeys();
-    setUpLight();
- 
-    // We load the scene from the zip file and adjust its size.
-    sceneModel = assetManager.loadModel("Scenes/map_005.j3o");
-    sceneModel.setLocalScale(8f);
-    /*Spatial sky = SkyFactory.createSky(assetManager, "Textures/tex_sky_001.png", true);
-    sky.setLocalScale(0.05f);
-    rootNode.attachChild(sky);*/
- 
-    // We set up collision detection for the scene by creating a
-    // compound collision shape and a static RigidBodyControl with mass zero.
-    CollisionShape sceneShape =
-            CollisionShapeFactory.createMeshShape((Node) sceneModel);
-    landscape = new RigidBodyControl(sceneShape, 0);
-    sceneModel.addControl(landscape);
- 
-    // We set up collision detection for the player by creating
-    // a capsule collision shape and a CharacterControl.
-    // The CharacterControl offers extra settings for
-    // size, stepheight, jumping, falling, and gravity.
-    // We also put the player in its starting position.
-    CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
-    player = new CharacterControl(capsuleShape, 1.0f);
-    player.setJumpSpeed(10f);
-    player.setFallSpeed(20f);
-    player.setGravity(20f);
-    player.setMaxSlope(0.8f);
-    player.setPhysicsLocation(new Vector3f(0, 128, 0));
- 
-    // We attach the scene and the player to the rootNode and the physics space,
-    // to make them appear in the game world.
-    rootNode.attachChild(sceneModel);
-    bulletAppState.getPhysicsSpace().add(landscape);
-    bulletAppState.getPhysicsSpace().add(player);
-  }
- 
-  private void setUpLight() {
-    // We add light so we see the scene
-    AmbientLight al = new AmbientLight();
-    al.setColor(ColorRGBA.White.mult(1.7f));
-    rootNode.addLight(al);
-
-  }
- 
-  /** We over-write some navigational key mappings here, so we can
-   * add physics-controlled walking and jumping: */
-  private void setUpKeys() {
-    inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
-    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
-    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
-    inputManager.addListener(this, "Left");
-    inputManager.addListener(this, "Right");
-    inputManager.addListener(this, "Up");
-    inputManager.addListener(this, "Down");
-    inputManager.addListener(this, "Jump");
-  }
- 
-  /** These are our custom actions triggered by key presses.
-   * We do not walk yet, we just keep track of the direction the user pressed. */
-  public void onAction(String binding, boolean value, float tpf) {
-    if (binding.equals("Left")) {
-      left = value;
-    } else if (binding.equals("Right")) {
-      right = value;
-    } else if (binding.equals("Up")) {
-      up = value;
-    } else if (binding.equals("Down")) {
-      down = value;
-    } else if (binding.equals("Jump")) {
-      player.jump();
+  
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
     }
-  }
  
-  /**
-   * This is the main event loop--walking happens here.
-   * We check in which direction the player is walking by interpreting
-   * the camera direction forward (camDir) and to the side (camLeft).
-   * The setWalkDirection() command is what lets a physics-controlled player walk.
-   * We also make sure here that the camera moves with player.
-   */
-  @Override
-  public void simpleUpdate(float tpf) {
-    Vector3f camDir = cam.getDirection().clone().multLocal(0.2f);
-    Vector3f camLeft = cam.getLeft().clone().multLocal(0.2f);
-    walkDirection.set(0, 0, 0);
-    if (left)  { walkDirection.addLocal(camLeft); }
-    if (right) { walkDirection.addLocal(camLeft.negate()); }
-    if (up)    { walkDirection.addLocal(camDir); }
-    if (down)  { walkDirection.addLocal(camDir.negate()); }
-    player.setWalkDirection(walkDirection);
-    cam.setLocation(player.getPhysicsLocation());
-  }
+    public void simpleInitApp() {
+        // Set up Physics
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        bulletAppState.getPhysicsSpace().enableDebug(assetManager);
+ 
+        viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
+    
+        FilterPostProcessor filter = new FilterPostProcessor(assetManager);
+        FogFilter fog = new FogFilter();
+        fog.setFogColor(new ColorRGBA(0.9f, 0.9f, 0.9f, 1.0f));
+        fog.setFogDistance(512);
+        fog.setFogDensity(1.5f);
+        filter.addFilter(fog);
+        viewPort.addProcessor(filter);
+    
+        flyCam.setMoveSpeed(100);
+        setUpKeys();
+        setUpLight();
+ 
+        sceneModel = assetManager.loadModel("Scenes/map_005.j3o");
+        sceneModel.setLocalScale(8f);
+ 
+        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape((Node) sceneModel);
+        landscape = new RigidBodyControl(sceneShape, 0);
+        sceneModel.addControl(landscape);
+ 
+        CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        player = new CharacterControl(capsuleShape, 1.0f);
+        player.setJumpSpeed(10f);
+        player.setFallSpeed(20f);
+        player.setGravity(20f);
+        player.setMaxSlope(0.8f);
+        player.setPhysicsLocation(new Vector3f(0, 128, 0));
+
+        rootNode.attachChild(sceneModel);
+        bulletAppState.getPhysicsSpace().add(landscape);
+        bulletAppState.getPhysicsSpace().add(player);
+    }
+ 
+    private void setUpLight() {
+        AmbientLight al = new AmbientLight();
+        al.setColor(ColorRGBA.White.mult(1.7f));
+        rootNode.addLight(al);
+    }
+ 
+    private void setUpKeys() {
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(this, "Left");
+        inputManager.addListener(this, "Right");
+        inputManager.addListener(this, "Up");
+        inputManager.addListener(this, "Down");
+        inputManager.addListener(this, "Jump");
+    }
+ 
+    public void onAction(String binding, boolean value, float tpf) {
+        if (binding.equals("Left")) {
+            left = value;
+        } else if (binding.equals("Right")) {
+            right = value;
+        } else if (binding.equals("Up")) {
+            up = value;
+        } else if (binding.equals("Down")) {
+            down = value;
+        } else if (binding.equals("Jump")) {
+            player.jump();
+        }
+    }
+ 
+    @Override
+    public void simpleUpdate(float tpf) {
+        Vector3f camDir = cam.getDirection().clone().multLocal(0.2f);
+        Vector3f camLeft = cam.getLeft().clone().multLocal(0.2f);
+        walkDirection.set(0, 0, 0);
+        if (left)  { walkDirection.addLocal(camLeft); }
+        if (right) { walkDirection.addLocal(camLeft.negate()); }
+        if (up)    { walkDirection.addLocal(camDir); }
+        if (down)  { walkDirection.addLocal(camDir.negate()); }
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getPhysicsLocation());
+    }
 }
